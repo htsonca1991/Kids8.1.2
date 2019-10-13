d1c34c0ff30194f605044001b4827173bb00b0c4
htsonca1991 <51393926+htsonca1991@users.noreply.github.com>
2019-10-14 01:20:23 +0700

    222

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: KidsPrivate\nLabel: KidsPrivate\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: KidsAuto iOS for Device Model 10.x - 12.4\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
+printf "Origin: KidsFunny\nLabel: KidsFunny\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: KidsFunny iOS\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
 exit 0
